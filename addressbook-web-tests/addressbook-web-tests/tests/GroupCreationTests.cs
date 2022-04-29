2ac398a62e9074217baf6254a47de20626dd2c6a
Alena <chalenkan@gmail.com>
2022-04-29 15:33:54 +0300

    Add check for group creation

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace WebAddressbookTests
@@ -16,7 +17,12 @@ namespace WebAddressbookTests
             group.Header = "Test2Header";
             group.Footer = "Test2Footer";
 
+            List<GroupData> oldGroups = app.GroupHelper.GetGroupList();
+
             app.GroupHelper.Create(group);
+
+            List<GroupData> newGroups = app.GroupHelper.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);  
         }
 
         [Test]
@@ -26,7 +32,27 @@ namespace WebAddressbookTests
             group.Header = "";
             group.Footer = "";
 
+            List<GroupData> oldGroups = app.GroupHelper.GetGroupList();
+
             app.GroupHelper.Create(group);
+
+            List<GroupData> newGroups = app.GroupHelper.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+        }
+
+        [Test]
+        public void BadNameGroupCreationTest()
+        {
+            GroupData group = new GroupData("a'");
+            group.Header = "";
+            group.Footer = "";
+
+            List<GroupData> oldGroups = app.GroupHelper.GetGroupList();
+
+            app.GroupHelper.Create(group);
+
+            List<GroupData> newGroups = app.GroupHelper.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
         }
     }
 }
