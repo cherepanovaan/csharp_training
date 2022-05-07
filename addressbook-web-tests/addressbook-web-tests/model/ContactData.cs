57c99a584436465e8ffea6ff7a0a94a8ca7398e0
Alena <chalenkan@gmail.com>
2022-05-07 20:19:52 +0300

    Add TestContactInformation

--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -8,6 +8,9 @@ namespace WebAddressbookTests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
+        private string allEmails;
+        private string allPhones;
+
         public ContactData(string firstName, string lastName)
         {
             FirstName = firstName;
@@ -24,12 +27,14 @@ namespace WebAddressbookTests
             {
                 return true;
             }
-            return FirstName == other.FirstName && LastName == other.LastName;
+            return FirstName == other.FirstName 
+                && LastName == other.LastName;
         }
 
         public override int GetHashCode()
         {
-            return FirstName.GetHashCode() & LastName.GetHashCode();
+            return FirstName.GetHashCode() 
+                & LastName.GetHashCode();
         }
 
         public override string ToString()
@@ -51,5 +56,67 @@ namespace WebAddressbookTests
         public string LastName { get; set; } 
 
         public string Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string Email2 { get; set; }
+
+        public string Email3 { get; set; }
+
+        public string AllEmails
+        {
+            get
+            {
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+               else
+                {
+                    return Email + "\r\n" + Email2 + "\r\n" + Email3;
+                }
+            }
+            set
+            {
+                allEmails = value;
+            }
+        }
+
+        public string HomePhone { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string WorkPhone { get; set; }
+
+        public string AllPhones 
+        { 
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
+                }
+            }
+            set
+            {
+                allPhones = value;
+            }
+        }
+
+        private string CleanUp(string phone)
+        {
+            if (phone == null || phone == "")
+            {
+                return "";
+
+            }
+            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
     }
 }
