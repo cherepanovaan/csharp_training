785189588f82b9de46830e18f348944664bf2832
cherepanovaan <chalenkan@gmail.com>
2022-05-29 15:47:37 +0300

    Fix RemovingContactFromGroup

--- addressbook-web-tests/addressbook-web-tests/tests/RemovingContactFromGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/RemovingContactFromGroupTests.cs
@@ -12,8 +12,27 @@ namespace WebAddressbookTests
         [Test]
         public void TestRemovingContactFromGroup()
         {
-            GroupData group = GroupData.GetAll()[1];
+            if (GroupData.GetAll().Count == 0)
+            {
+                app.GroupHelper.Create(new GroupData(GenerateRandomString(30)));
+            }
+
+            GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldList = group.GetContacts();
+
+            if (oldList.Count == 0)
+            {
+                List<ContactData> contacts = ContactData.GetAll();
+
+                if (contacts.Count == 0)
+                {
+                    app.ContactHelper.Create(new ContactData(GenerateRandomString(30), GenerateRandomString(30)));
+                }
+                
+                ContactData contact = ContactData.GetAll().First();
+                app.ContactHelper.AddContactToGroup(contact, group);
+                oldList = group.GetContacts();
+            }
 
             app.ContactHelper.RemoveContactFromGroup(oldList[0], group);
 
